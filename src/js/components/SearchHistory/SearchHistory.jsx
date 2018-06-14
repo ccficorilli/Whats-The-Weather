345bb153ba7b7121441562c6d56703c2203fa236
ccficorilli <ccficorilli@gmail.com>
2018-06-13 22:35:35 -0700

    styling is done added dark background option

--- src/js/components/SearchHistory/SearchHistory.jsx
+++ src/js/components/SearchHistory/SearchHistory.jsx
@@ -2,36 +2,34 @@ import React from 'react';
 
 class SearchHistory extends React.Component {
 
-    render(){
-        return(
-            <div className="history-container">
-                <div className="label">Search History</div>
-                <div className="history-display">
-                    <div className="history-row">
-                        <div className="prev-city">last city</div>
-                        <div className="date-searched">
-                            <div className="date">01/01/2001</div>
-                            <div className="time">05:05:55</div>
-                        </div>
-                    </div>
-                    <div className="history-row">
-                        <div className="prev-city">last city</div>
-                        <div className="date-searched">
-                            <div className="date">01/01/2001</div>
-                            <div className="time">05:05:55</div>
-                        </div>
-                    </div>
-                    <div className="history-row">
-                        <div className="prev-city">last city</div>
-                        <div className="date-searched">
-                            <div className="date">01/01/2001</div>
-                            <div className="time">05:05:55</div>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className='history-container'>
+        <div className='label'>Search History</div>
+          <div className='history-row dark'>
+            <div className='prev-city'>last city</div>
+            <div className='date-searched'>
+              <div className='date'>01/01/2001</div>
+              <div className='time'>05:05:55</div>
             </div>
-        )
-    }
-   
+          </div>
+          <div className='history-row'>
+            <div className='prev-city'>last city</div>
+            <div className='date-searched'>
+              <div className='date'>01/01/2001</div>
+              <div className='time'>05:05:55</div>
+            </div>
+          </div>
+          <div className='history-row dark'>
+            <div className='prev-city'>last city</div>
+            <div className='date-searched'>
+              <div className='date'>01/01/2001</div>
+              <div className='time'>05:05:55</div>
+            </div>
+          </div>
+      </div>
+    );
+  }
+
 }
 export default SearchHistory;
